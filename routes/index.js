46b3ec6f6a07a4f285244a33e1fb77d00d3800f4
jamesGadoury <gadouryjames@gmail.com>
2020-06-07 16:59:00 -0400

    update css and grid representation in sim

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+class Cell {
+  constructor(vacuumPresent, dirtPresent) {
+    this.vacuumPresent = vacuumPresent;
+    this.dirtPresent   = dirtPresent;
+  }
+}
+
+function makeGrid(numRows, numCols) {
+  let grid = [];
+  for (let i = 0; i < numRows; ++i) {
+    for (let j = 0; j < numCols; ++j) {
+      grid[i*numCols + j] = new Cell(false, false);
+    }
+  }
+
+  //TODO fix
+  grid[0].vacuumPresent = true;
+  grid[1].dirtPresent = true;
+  grid[2] = new Cell(true, true);
+  return grid;
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let numRows = 3;
+  let numCols = 4;
+  let gridHeight = numRows * 50 + "px";
+  let colWidth = 100 / numCols + "%";
   res.render('index', { 
     title: 'Vacuum World', 
     writeup: 'Hello User',
-    numRows: 1,
-    numCols: 2 });
+    numRows: numRows,
+    numCols: numCols,
+    gridHeight: gridHeight,
+    colWidth: colWidth,
+    grid: makeGrid(numRows, numCols) 
+  });
 });
 
 module.exports = router;
