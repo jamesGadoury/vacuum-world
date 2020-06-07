5827d8a07b6ba76e4d5492ad4e8a4fe22095532b
jamesGadoury <gadouryjames@gmail.com>
2020-06-07 12:27:01 -0400

    implement ability to make simulation grid

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Vacuum World' });
+  res.render('index', { 
+    title: 'Vacuum World', 
+    writeup: 'Hello User',
+    numRows: 1,
+    numCols: 2 });
 });
 
 module.exports = router;
