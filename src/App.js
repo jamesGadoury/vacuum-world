1b87c4b97c4e085c2562f9d5609e4059b90f4f09
jamesGadoury <gadouryjames@gmail.com>
2020-06-13 16:16:54 -0400

    init React version of vacuum world

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h1>Vacuum World</h1>
+      </div>
+    </div>
+  );
+}
+
+
+
+export default App;
