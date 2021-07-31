5542cc944d52bb50c590ba3a91ae1ce5f5069102
James Gadoury <gadouryjames@gmail.com>
2021-07-30 21:50:38 -0400

    Make WorldSim a functional component, and move state up to SimManager

--- src/SimManager.js
+++ src/SimManager.js
@@ -3,6 +3,8 @@ import WorldSim from './WorldSim';
 import {initWorld} from './World';
 import { RobotTypes, CreateRobot } from './Robots';
 import { UniqueKeyGenerator } from './Utilities';
+import {updateWorld} from './World';
+import { Actions } from './Actions';
 
 class SimManager extends React.Component {
    constructor(props) {
@@ -14,7 +16,8 @@ class SimManager extends React.Component {
          numRows: props.numRows,
          numCols: props.numCols,
          runningSim: true,
-         keyGen: new UniqueKeyGenerator()
+         keyGen: new UniqueKeyGenerator(),
+         robotActionStr: "",
       };
 
       this.updateProperties(props.numRows, props.numCols);
@@ -25,11 +28,68 @@ class SimManager extends React.Component {
       document.documentElement.style.setProperty("--numCols", numCols);
       document.documentElement.style.setProperty("--colWidth", 100 / numCols + "%");
    }
+   
+
+   stepSimulation = () => {
+      let action = this.state.robot.nextAction(this.state.world);
+      if (Actions.indexOf(action) === -1)
+         console.log(`${action} is not a valid action.`);
+      this.setState({
+         world: updateWorld(this.state.world, action),
+         robotActionStr:action,
+      });
+   }
+
+   timer() {
+      return setInterval(
+         () => this.stepSimulation(),
+         1000
+      );
+   }
+   startTimer() {
+      this.timerId = this.timer();
+   }
+   stopTimer() {
+      clearInterval(this.timerId);
+   }
+ 
+   componentDidMount() {
+      this.startTimer();
+   }
+
    componentDidUpdate(prevProps, prevState) {
+      let updatedState = false;
+      if (this.props.world !== prevProps.world) {
+         this.setState({
+            world: this.props.world
+         });
+         updatedState = true;
+      }
+      if (this.props.robot !== prevProps.robot) {
+         this.setState({
+            robot: this.props.robot
+         });
+         updatedState = true;
+      }
+      if (updatedState && this.props.runningSim) {
+         this.stopTimer();
+         this.startTimer();
+      }
+      if (this.props.runningSim !== prevProps.runningSim) {
+         if (this.props.runningSim) {
+            this.startTimer();
+         } else {
+            this.stopTimer();
+         }
+      }
       if (this.state.numRows !== prevState.numRows || this.state.numCols !== prevState.numCols) {
          this.updateProperties(this.state.numRows, this.state.numCols);
          this.resetWorld();
       }
+   }
+ 
+   componentWillUnmount() {
+      this.stopTimer();
    }
 
    resetWorld = () => {
@@ -114,7 +174,7 @@ class SimManager extends React.Component {
 
    renderWorld() {
       return (
-         <WorldSim robot={this.state.robot} world={this.state.world} runningSim={this.state.runningSim}/>
+         <WorldSim robot={this.state.robot} world={this.state.world} robotActionStr={this.state.robotActionStr}/>
       );
    }
 
--- src/WorldSim.js
+++ src/WorldSim.js
@@ -1,105 +1,36 @@
 import React from 'react';
-import {updateWorld} from './World';
-import { Actions } from './Actions';
 import { UniqueKeyGenerator } from './Utilities';
 
-class WorldSim extends React.Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         world: props.world,
-         robot: props.robot,
-         robotActionStr: "",
-         keyGen: new UniqueKeyGenerator()
-      };
-   }
+function WorldSim(props)  {
+   let keyGen = new UniqueKeyGenerator(); // todo do i even need this thing
  
-   stepSimulation() {
-      let action = this.state.robot.nextAction(this.state.world);
-      if (Actions.indexOf(action) === -1)
-         console.log(`${action} is not a valid action.`);
-      this.setState({
-         world: updateWorld(this.state.world, action),
-         robotActionStr:action,
-      });
-   }
-   timer() {
-      return setInterval(
-         () => this.stepSimulation(),
-         1000
-      );
-   }
-   startTimer() {
-      this.timerId = this.timer();
-   }
-   stopTimer() {
-      clearInterval(this.timerId);
-   }
- 
-   componentDidMount() {
-      this.startTimer();
-   }
-
-   componentDidUpdate(prevProps) {
-      let updatedState = false;
-      if (this.props.world !== prevProps.world) {
-         this.setState({
-            world: this.props.world
-         });
-         updatedState = true;
-      }
-      if (this.props.robot !== prevProps.robot) {
-         this.setState({
-            robot: this.props.robot
-         });
-         updatedState = true;
-      }
-      if (updatedState && this.props.runningSim) {
-         this.stopTimer();
-         this.startTimer();
-      }
-      if (this.props.runningSim !== prevProps.runningSim) {
-         if (this.props.runningSim) {
-            this.startTimer();
-         } else {
-            this.stopTimer();
-         }
-      }
-   }
- 
-   componentWillUnmount() {
-      this.stopTimer();
-   }
- 
-   renderVacuum(cell) {
+   const renderVacuum = (cell) => {
       if (cell.vacuumPresent) {
          return (
-         <div className='vacuum'><p className='action'>{this.state.robotActionStr}</p></div>
+         <div className='vacuum'><p className='action'>{props.robotActionStr}</p></div>
          )
       }
       return;
    }
 
-   renderCell(cell) {
+   const renderCell = (cell) => {
       if (cell.dirtPresent) {
-         return <div className='sim-cell' style={{'backgroundColor':'SaddleBrown'}} key={this.state.keyGen.key()}>{this.renderVacuum(cell)}</div>;
+         return <div className='sim-cell' style={{'backgroundColor':'SaddleBrown'}} key={keyGen.key()}>{renderVacuum(cell)}</div>;
       }
-      return <div className='sim-cell' style={{'backgroundColor':'Cornsilk'}} key={this.state.keyGen.key()}>{this.renderVacuum(cell)}</div>
+      return <div className='sim-cell' style={{'backgroundColor':'Cornsilk'}} key={keyGen.key()}>{renderVacuum(cell)}</div>
    }
 
-   renderCells() {
+   const renderCells = () => {
       return (
-         this.state.world.grid.map((cell) => this.renderCell(cell))
+         props.world.grid.map((cell) => renderCell(cell))
       );
    }
  
-   render() {
-      return (
-         <div className='div-sim'>
-            {this.renderCells()}
-         </div>
-      );
-   }
+   return (
+      <div className='div-sim'>
+         {renderCells()}
+      </div>
+   );
 }
 
 export default WorldSim;
